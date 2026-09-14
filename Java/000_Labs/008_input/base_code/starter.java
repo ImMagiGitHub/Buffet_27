6d896262d2d2be9dfd4673e2357175b76f0cb814
ImMagi <magicraftplays@gmail.com>
2026-09-14 21:58:54 +0000

    update

--- Java/000_Labs/008_input/base_code/starter.java
+++ Java/000_Labs/008_input/base_code/starter.java
@@ -1,14 +1,24 @@
-/*
- *	Author:  
- *  Date: 
-*/
-
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Whats your name?");
+		String anwser1 = sc.nextLine();
+		System.out.println("How old are you?");
+		int anwser2 = sc.nextInt();
+		System.out.println("What month were you born? (1-12)");
+		int anwser3 = sc.nextInt();
+		System.out.println("What day were you born? (1-31)");
+		int anwser4 = sc.nextInt();
+		System.out.println("What year were you born?");
+		int anwser5 = sc.nextInt();
+		System.out.println("If you have 1 dollar and 2 quarters, how much mulla do you have?");
+		double anwser6 = sc.nextDouble();
+		System.out.println("Your name is " + anwser1 + " and you were born " + anwser3 + "/" + anwser4 + "/" + anwser5 + ".");
+		System.out.println("You are " + anwser2 + " years old :]");
+		System.out.println("and I just stole your last " + anwser6 + " in your bank account.. MUHAHAHAHAHAH!!");
 	}
 }
