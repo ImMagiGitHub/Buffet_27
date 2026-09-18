33753d7056848965e556c79f190557f14b643962
ImMagi <magicraftplays@gmail.com>
2026-09-18 21:39:58 +0000

    update

--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -1,15 +1,19 @@
-/*
- *	Author:  
- *  Date: 
-*/
-
 import java.util.Scanner;
 import java.util.Random;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+
+	Scanner sc = new Scanner(System.in);
+	
+	int blah = (int)(Math.random()*9);
+	int bluh = (int)(Math.random()*99+1);
+	double bleh = Math.random()*0.5+2.5;
+	double bloh = Math.random()*575+14;
+
+	System.out.println("A number between 0 - 9:  " + blah);
+	System.out.println("A number between 1 - 100: " + bluh);
+	System.out.println("A double between 2.5 and 3.5:  " + bleh);
+	System.out.println("A double between 14 and 589:   " + bloh);
 	}
 }
--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,60 @@
-/*
-    Lecture note example - Input!!
-*/
+import java.util.Scanner;
 
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
-	}
+
+        Scanner sc = new Scanner(System.in);
+
+        double duckTaco = 2.25;
+        double duckHorchata = 1.50;
+        double duckSalad = 1.00;
+        double duckSoup = 1.75;
+        double duckBurger = 3.50;
+        double duckSalsa = 0.25;
+        double duckRanch = 0.15;
+        System.out.println("-Duck Butcher-");
+        System.out.println(" ");
+        System.out.println("Daily Special: " + duckHorchata);
+        System.out.println("Apitizers:");
+        System.out.println(" ");
+        System.out.println("Duck Salad: " + duckSalad);
+        System.out.println("Duck Soup: " + duckSoup);
+        System.out.println("Duck Salsa: " + duckSalsa);
+        System.out.println("Duck Ranch: " + duckRanch);
+        System.out.println("Main courses:");
+        System.out.println(" ");
+        System.out.println("Duck Taco: " + duckTaco);
+        System.out.println("Duck Burger: " + duckBurger );
+
+        System.out.println(" ");
+
+        System.out.println("Welcome to Duck Butcher! Who will this order be for?");
+        String name = sc.nextLine();
+        System.out.println("How much Duck Horchata would you like?");
+        int horchata = sc.nextInt();
+        System.out.println("How much Duck Salad would you like?"); 
+        int salad = sc.nextInt();
+        System.out.println("How much Duck Soup would you like?");
+        int soup = sc.nextInt();
+        System.out.println("How much Duck Salsa would you like?");
+        int salsa = sc.nextInt();
+        System.out.println("How much Duck Ranch would you like?");
+        int ranch = sc.nextInt();
+        System.out.println("How much Duck Taco would you like?");
+        int taco = sc.nextInt();
+        System.out.println("How much Duck Burger would you like?");
+        int burger = sc.nextInt();
+
+        double horchataPrice = duckHorchata * horchata;
+        double saladPrice = duckSalad * salad;
+        double soupPrice = duckSoup * soup;
+        double salsaPrice = duckSalsa * salsa;
+        double ranchPrice = duckRanch * ranch;
+        double tacoPrice = duckTaco * taco;
+        double burgerPrice = duckBurger * burger;
+        System.out.println("The order for " + name + " is ready! Your total is " + (horchataPrice + saladPrice + soupPrice + salsaPrice + ranchPrice + tacoPrice + burgerPrice));
+        double tip = sc.nextDouble();
+        tip = tip/100*(horchataPrice + saladPrice + soupPrice + salsaPrice + ranchPrice + tacoPrice + burgerPrice));
+
+    }
 }
